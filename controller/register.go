0b4ec80b12a884d99b99e675c694b5407fda9c03
ryaasishlah <ryaasishlah123@gmail.com>
2024-07-06 19:05:49 +0700

    add register function controller

---
+++ controller/register.go
@@ -0,0 +1,74 @@
+package controller
+
+import (
+	"crypto/rand"
+	"encoding/hex"
+	"encoding/json"
+	"net/http"
+	"strings"
+
+	"github.com/allrole-ai/backend-ai/helper"
+	model "github.com/allrole-ai/backend-ai/model"
+	"github.com/badoux/checkmail"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+	"golang.org/x/crypto/argon2"
+)
+
+func Register(db *mongo.Database, col string, respw http.ResponseWriter, req *http.Request) {
+	var user model.User
+
+	err := json.NewDecoder(req.Body).Decode(&user)
+	if err != nil {
+		helper.ErrorResponse(respw, req, http.StatusBadRequest, "Bad Request", "error parsing request body "+err.Error())
+		return
+	}
+
+	if user.NamaLengkap == "" || user.Email == "" || user.Password == "" || user.Confirmpassword == "" {
+		helper.ErrorResponse(respw, req, http.StatusBadRequest, "Bad Request", "dimohon untuk melengkapi data")
+		return
+	}
+	if err := checkmail.ValidateFormat(user.Email); err != nil {
+		helper.ErrorResponse(respw, req, http.StatusBadRequest, "Bad Request", "email tidak valid")
+		return
+	}
+	userExists, _ := helper.GetUserFromEmail(user.Email, db)
+	if user.Email == userExists.Email {
+		helper.ErrorResponse(respw, req, http.StatusBadRequest, "Bad Request", "email sudah terdaftar")
+		return
+	}
+	if strings.Contains(user.Password, " ") {
+		helper.ErrorResponse(respw, req, http.StatusBadRequest, "Bad Request", "password tidak boleh terdapat spasi")
+		return
+	}
+	if len(user.Password) < 8 {
+		helper.ErrorResponse(respw, req, http.StatusBadRequest, "Bad Request", "password minimal 8 karakter")
+		return
+	}
+	salt := make([]byte, 16)
+	_, err = rand.Read(salt)
+	if err != nil {
+		helper.ErrorResponse(respw, req, http.StatusInternalServerError, "Internal Server Error", "kesalahan server : salt")
+		return
+	}
+	hashedPassword := argon2.IDKey([]byte(user.Password), salt, 1, 64*1024, 4, 32)
+	userData := bson.M{
+		"namalengkap": user.NamaLengkap,
+		"email":       user.Email,
+		"password":    hex.EncodeToString(hashedPassword),
+		"salt":        hex.EncodeToString(salt),
+	}
+	insertedID, err := helper.InsertOneDoc(db, col, userData)
+	if err != nil {
+		helper.ErrorResponse(respw, req, http.StatusInternalServerError, "Internal Server Error", "kesalahan server : insert data, "+err.Error())
+		return
+	}
+	resp := map[string]any{
+		"message":    "berhasil mendaftar",
+		"insertedID": insertedID,
+		"data": map[string]string{
+			"email": user.Email,
+		},
+	}
+	helper.WriteJSON(respw, http.StatusCreated, resp)
+}
