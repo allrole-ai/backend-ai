fde9f0f6699c4c4697ed850b2496bfb1db610c67
miqbalramadhan18 <miqbalr18@gmail.com>
2024-07-26 16:24:09 +0700

    Remove retry logic for Hugging Face API requests

--- controller/chat.go
+++ controller/chat.go
@@ -4,8 +4,6 @@ import (
 	"encoding/json"
 	"log"
 	"net/http"
-	"net/url"
-	"strings"
 	"time"
 
 	"github.com/allrole-ai/backend-ai/config"
@@ -38,49 +36,12 @@ func Chat(respw http.ResponseWriter, req *http.Request, tokenmodel string) {
 	apiUrl := config.GetEnv("HUGGINGFACE_API_KEY")
 	apiToken := "Bearer " + tokenmodel
 
-	parsedURL, err := url.Parse(apiUrl)
-	if err != nil {
-		helper.ErrorResponse(respw, req, http.StatusInternalServerError, "Internal Server Error", "error parsing URL model Hugging Face: "+err.Error())
-		return
-	}
-
-	segments := strings.Split(parsedURL.Path, "/")
-	modelName := strings.Join(segments[2:], "/")
-
-	// Request ke API Hugging Face dengan retry logic
 	var response *resty.Response
-	retryCount := 0
+	var retryCount int
 	maxRetries := 5
 	retryDelay := 20 * time.Second
 
-	for retryCount < maxRetries {
-		response, err = client.R().
-			SetHeader("Authorization", apiToken).
-			SetHeader("Content-Type", "application/json").
-			SetBody(`{"inputs": "` + chat.Query + `"}`).
-			Post(apiUrl)
-
-		if err != nil {
-			log.Printf("Error making request: %v", err)
-			retryCount++
-			time.Sleep(retryDelay)
-			continue
-		}
-
-		if response.StatusCode() == http.StatusOK {
-			break
-		} else {
-			var errorResponse map[string]interface{}
-			err = json.Unmarshal(response.Body(), &errorResponse)
-			if err == nil && errorResponse["error"] == "Model "+modelName+" is currently loading" {
-				retryCount++
-				time.Sleep(retryDelay)
-				continue
-			}
-			helper.ErrorResponse(respw, req, http.StatusInternalServerError, "Internal Server Error", "error from Hugging Face API: "+string(response.Body()))
-			return
-		}
-	}
+	
 
 	if response.StatusCode() != http.StatusOK {
 		helper.ErrorResponse(respw, req, http.StatusInternalServerError, "Internal Server Error", "error from Hugging Face API: "+string(response.Body()))
