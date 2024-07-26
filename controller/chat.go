1ee111bad909d8b0f059639884c2a498d2de939c
miqbalramadhan18 <miqbalr18@gmail.com>
2024-07-26 16:50:34 +0700

    update chat pada controller

--- controller/chat.go
+++ controller/chat.go
@@ -4,6 +4,8 @@ import (
 	"encoding/json"
 	"log"
 	"net/http"
+	"net/url"
+	"strings"
 	"time"
 
 	"github.com/allrole-ai/backend-ai/config"
@@ -21,14 +23,10 @@ func Chat(respw http.ResponseWriter, req *http.Request, tokenmodel string) {
 		return
 	}
 
-	if chat.Prompt == "" {
+	if chat.Query == "" {
 		helper.ErrorResponse(respw, req, http.StatusBadRequest, "Bad Request", "mohon untuk melengkapi data")
 		return
 	}
-	// if chat.Query == "" {
-	// 	helper.ErrorResponse(respw, req, http.StatusBadRequest, "Bad Request", "mohon untuk melengkapi data")
-	// 	return
-	// }
 
 	client := resty.New()
 
@@ -36,45 +34,56 @@ func Chat(respw http.ResponseWriter, req *http.Request, tokenmodel string) {
 	apiUrl := config.GetEnv("HUGGINGFACE_API_KEY")
 	apiToken := "Bearer " + tokenmodel
 
+	parsedURL, err := url.Parse(apiUrl)
+	if err != nil {
+		helper.ErrorResponse(respw, req, http.StatusInternalServerError, "Internal Server Error", "error parsing URL model Hugging Face: "+err.Error())
+		return
+	}
+
+	segments := strings.Split(parsedURL.Path, "/")
+	modelName := strings.Join(segments[2:], "/")
+
+	// Request ke API Hugging Face dengan retry logic
 	var response *resty.Response
-	var retryCount int
+	retryCount := 0
 	maxRetries := 5
 	retryDelay := 20 * time.Second
 
-		// Request ke Hugging Face API
-		for retryCount < maxRetries {
-			response, err = client.R().
-				SetHeader("Authorization", apiToken).
-				SetHeader("Content-Type", "application/json").
-				SetBody(`{"inputs": "` + chat.Prompt + `"}`).
-				Post(apiUrl)
-	
-			if err != nil {
-				log.Fatalf("Error making request: %v", err)
-			}
-	
-			if response.StatusCode() == http.StatusOK {
-				break
-			} else {
-				var errorResponse map[string]interface{}
-				err = json.Unmarshal(response.Body(), &errorResponse)
-				if err == nil && errorResponse["error"] == "Model is currently loading" {
-					retryCount++
-					time.Sleep(retryDelay)
-					continue
-				}
-				helper.ErrorResponse(respw, req, http.StatusInternalServerError, "Internal Server Error", "error from Hugging Face API "+string(response.Body()))
-				return
-			}
+	for retryCount < maxRetries {
+		response, err = client.R().
+			SetHeader("Authorization", apiToken).
+			SetHeader("Content-Type", "application/json").
+			SetBody(`{"inputs": "` + chat.Query + `"}`).
+			Post(apiUrl)
+
+		if err != nil {
+			log.Printf("Error making request: %v", err)
+			retryCount++
+			time.Sleep(retryDelay)
+			continue
 		}
 
-		if response.StatusCode() != 200 {
-			helper.ErrorResponse(respw, req, http.StatusInternalServerError, "Internal Server Error", "error from Hugging Face API "+string(response.Body()))
+		if response.StatusCode() == http.StatusOK {
+			break
+		} else {
+			var errorResponse map[string]interface{}
+			err = json.Unmarshal(response.Body(), &errorResponse)
+			if err == nil && errorResponse["error"] == "Model "+modelName+" is currently loading" {
+				retryCount++
+				time.Sleep(retryDelay)
+				continue
+			}
+			helper.ErrorResponse(respw, req, http.StatusInternalServerError, "Internal Server Error", "error from Hugging Face API: "+string(response.Body()))
 			return
 		}
+	}
+
+	if response.StatusCode() != http.StatusOK {
+		helper.ErrorResponse(respw, req, http.StatusInternalServerError, "Internal Server Error", "error from Hugging Face API: "+string(response.Body()))
+		return
+	}
 
 	var data []map[string]interface{}
-
 	err = json.Unmarshal(response.Body(), &data)
 	if err != nil {
 		helper.ErrorResponse(respw, req, http.StatusInternalServerError, "Internal Server Error", "error parsing response body: "+err.Error())
@@ -82,13 +91,14 @@ func Chat(respw http.ResponseWriter, req *http.Request, tokenmodel string) {
 	}
 
 	// Logging untuk memeriksa struktur data yang diterima
+	log.Printf("Response data: %v", data)
+
 	if len(data) > 0 {
-		generatedText, ok := data[0]["generated_text"].(string)
-		if !ok {
-			helper.ErrorResponse(respw, req, http.StatusInternalServerError, "Internal Server Error", "error extracting generated text")
+		if answer, ok := data[0]["answer"].(string); ok {
+			helper.WriteJSON(respw, http.StatusOK, map[string]string{"answer": answer})
 			return
 		}
-		helper.WriteJSON(respw, http.StatusOK, map[string]string{"answer": generatedText})
+		helper.ErrorResponse(respw, req, http.StatusInternalServerError, "Internal Server Error", "error extracting answer")
 	} else {
 		helper.ErrorResponse(respw, req, http.StatusInternalServerError, "Internal Server Error", "kesalahan server: response")
 	}
