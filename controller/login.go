e8ecd31d6f53eaa178bc07c0dc6f61d3cf42be9c
FarhanRiziq01 <farhanriziq01@gmail.com>
2024-07-29 23:07:16 +0700

    add helper error response

--- controller/login.go
+++ controller/login.go
@@ -9,3 +9,8 @@ func Login(db *mongo.Database, respw http.ResponseWriter, req *http.Request, pri
 		helper.ErrorResponse(respw, req, http.StatusBadRequest, "Bad Request", "error parsing request body "+err.Error())
 		return
 	}
+
+	if user.Email == "" || user.Password == "" {
+		helper.ErrorResponse(respw, req, http.StatusBadRequest, "Bad Request", "mohon untuk melengkapi data")
+		return
+	}
