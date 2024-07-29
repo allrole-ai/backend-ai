4b7709facdef373041b73056f0be4ff178f6aedb
FarhanRiziq01 <farhanriziq01@gmail.com>
2024-07-29 23:07:42 +0700

    add helper bad req

--- controller/login.go
+++ controller/login.go
@@ -14,3 +14,8 @@ func Login(db *mongo.Database, respw http.ResponseWriter, req *http.Request, pri
 		helper.ErrorResponse(respw, req, http.StatusBadRequest, "Bad Request", "mohon untuk melengkapi data")
 		return
 	}
+
+	if err := checkmail.ValidateFormat(user.Email); err != nil {
+		helper.ErrorResponse(respw, req, http.StatusBadRequest, "Bad Request", "email tidak valid")
+		return
+	}
