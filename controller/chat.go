396f6810f517aa78594ba608a941f567a63ac52b
FarhanRiziq01 <farhanriziq01@gmail.com>
2024-07-29 22:39:18 +0700

    delete helper error response

--- controller/chat.go
+++ controller/chat.go
@@ -1,12 +1,1 @@
 package controller
-
-
-
-
-
-
-		helper.WriteJSON(respw, http.StatusOK, map[string]string{"answer": generatedText})
-	} else {
-		helper.ErrorResponse(respw, req, http.StatusInternalServerError, "Internal Server Error", "kesalahan server: response")
-	}
-}
