eb261d87e5ae0f55031732417414a7f8840f5191
FarhanRiziq01 <farhanriziq01@gmail.com>
2024-07-29 22:45:22 +0700

    add helper

--- controller/chat.go
+++ controller/chat.go
@@ -51,3 +51,7 @@ func Chat(respw http.ResponseWriter, req *http.Request, tokenmodel string) {
 				time.Sleep(retryDelay)
 				continue
 			}
+			helper.ErrorResponse(respw, req, http.StatusInternalServerError, "Internal Server Error", "error from Hugging Face API "+string(response.Body()))
+			return
+		}
+	}
