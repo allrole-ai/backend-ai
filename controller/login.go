479a4741d31aecb21400724e276171982e3ede87
FarhanRiziq01 <farhanriziq01@gmail.com>
2024-07-29 23:06:04 +0700

    delete respon

--- controller/login.go
+++ controller/login.go
@@ -1,16 +1,1 @@
 package controller
-
-
-
-
-	resp := map[string]any{
-		"status":  "success",
-		"message": "login berhasil",
-		"token":   tokenstring,
-		"data": map[string]string{
-			"email":       existsDoc.Email,
-			"namalengkap": existsDoc.NamaLengkap,
-		},
-	}
-	helper.WriteJSON(respw, http.StatusOK, resp)
-}
