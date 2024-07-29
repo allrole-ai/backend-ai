434f5547e8afe8c8740b0dfb7b7fa49903da5011
FarhanRiziq01 <farhanriziq01@gmail.com>
2024-07-29 23:04:00 +0700

    delete bad request

--- controller/login.go
+++ controller/login.go
@@ -1,10 +1,7 @@
 package controller
 
 
-	if user.Email == "" || user.Password == "" {
-		helper.ErrorResponse(respw, req, http.StatusBadRequest, "Bad Request", "mohon untuk melengkapi data")
-		return
-	}
+
 	if err = checkmail.ValidateFormat(user.Email); err != nil {
 		helper.ErrorResponse(respw, req, http.StatusBadRequest, "Bad Request", "email tidak valid")
 		return
