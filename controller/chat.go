8e2647f86ca6c434763add0e5d99528213da5904
FarhanRiziq01 <farhanriziq01@gmail.com>
2024-07-29 22:39:03 +0700

    delete helper error response

--- controller/chat.go
+++ controller/chat.go
@@ -4,12 +4,7 @@ package controller
 
 
 
-	if len(data) > 0 {
-		generatedText, ok := data[0]["generated_text"].(string)
-		if !ok {
-			helper.ErrorResponse(respw, req, http.StatusInternalServerError, "Internal Server Error", "error extracting generated text")
-			return
-		}
+
 		helper.WriteJSON(respw, http.StatusOK, map[string]string{"answer": generatedText})
 	} else {
 		helper.ErrorResponse(respw, req, http.StatusInternalServerError, "Internal Server Error", "kesalahan server: response")
