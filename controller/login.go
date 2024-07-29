412dded09a3ae935e052e04c216b3027c9b261bb
FarhanRiziq01 <farhanriziq01@gmail.com>
2024-07-29 23:08:13 +0700

    add helper internal server

--- controller/login.go
+++ controller/login.go
@@ -19,3 +19,9 @@ func Login(db *mongo.Database, respw http.ResponseWriter, req *http.Request, pri
 		helper.ErrorResponse(respw, req, http.StatusBadRequest, "Bad Request", "email tidak valid")
 		return
 	}
+
+	existsDoc, err := helper.GetUserFromEmail(user.Email, db)
+	if err != nil {
+		helper.ErrorResponse(respw, req, http.StatusInternalServerError, "Internal Server Error", "kesalahan server : get email "+err.Error())
+		return
+	}
