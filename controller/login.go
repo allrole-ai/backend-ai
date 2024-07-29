e97663c23c469953f89cfc4bd8ff69d2f5afebf1
FarhanRiziq01 <farhanriziq01@gmail.com>
2024-07-29 23:06:40 +0700

    add func login

--- controller/login.go
+++ controller/login.go
@@ -1,1 +1,11 @@
 package controller
+
+
+
+// Login handles user login
+func Login(db *mongo.Database, respw http.ResponseWriter, req *http.Request, privatekey string) {
+	var user model.User
+	if err := json.NewDecoder(req.Body).Decode(&user); err != nil {
+		helper.ErrorResponse(respw, req, http.StatusBadRequest, "Bad Request", "error parsing request body "+err.Error())
+		return
+	}
