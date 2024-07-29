1763afba4941dba6eb25413bb8b7912e31f6ecf0
FarhanRiziq01 <farhanriziq01@gmail.com>
2024-07-29 22:37:40 +0700

    delete helper

--- controller/chat.go
+++ controller/chat.go
@@ -3,10 +3,7 @@ package controller
 
 
 
-			helper.ErrorResponse(respw, req, http.StatusInternalServerError, "Internal Server Error", "error from Hugging Face API "+string(response.Body()))
-			return
-		}
-	}
+
 
 	if response.StatusCode() != 200 {
 		helper.ErrorResponse(respw, req, http.StatusInternalServerError, "Internal Server Errorr", "error from Hugging Face API "+string(response.Body()))
