4c3dcf7c4e48da167f4497647646d712c3ef15f5
FarhanRiziq01 <farhanriziq01@gmail.com>
2024-07-29 23:02:57 +0700

    delete func login error pharsing

--- controller/login.go
+++ controller/login.go
@@ -1,13 +1,6 @@
 package controller
 
-// user
-func Login(db *mongo.Database, respw http.ResponseWriter, req *http.Request, privatekey string) {
-	var user model.User
-	err := json.NewDecoder(req.Body).Decode(&user)
-	if err != nil {
-		helper.ErrorResponse(respw, req, http.StatusBadRequest, "Bad Request", "error parsing request body "+err.Error())
-		return
-	}
+
 	if user.Email == "" || user.Password == "" {
 		helper.ErrorResponse(respw, req, http.StatusBadRequest, "Bad Request", "mohon untuk melengkapi data")
 		return
