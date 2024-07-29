7105143425437a437a9176511f3d0dbdd68302a4
FarhanRiziq01 <farhanriziq01@gmail.com>
2024-07-29 22:46:47 +0700

    add helper error response

--- controller/chat.go
+++ controller/chat.go
@@ -68,3 +68,15 @@ func Chat(respw http.ResponseWriter, req *http.Request, tokenmodel string) {
 		helper.ErrorResponse(respw, req, http.StatusInternalServerError, "Internal Server Error", "error parsing response body "+err.Error())
 		return
 	}
+
+	if len(data) > 0 {
+		generatedText, ok := data[0]["generated_text"].(string)
+		if !ok {
+			helper.ErrorResponse(respw, req, http.StatusInternalServerError, "Internal Server Error", "error extracting generated text")
+			return
+		}
+		helper.WriteJSON(respw, http.StatusOK, map[string]string{"answer": generatedText})
+	} else {
+		helper.ErrorResponse(respw, req, http.StatusInternalServerError, "Internal Server Error", "kesalahan server: response")
+	}
+}
