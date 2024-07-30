32754d0d1fab9cc38c69cb74507ac3a2aa271df1
miqbalramadhan18 <miqbalr18@gmail.com>
2024-07-31 01:40:43 +0700

    Add error handling for database query

--- controller/login.go
+++ controller/login.go
@@ -28,7 +28,11 @@ func Login(db *mongo.Database, respw http.ResponseWriter, req *http.Request, pri
 	return
 
 	existsDoc, err := helper.GetUserFromEmail(user.Email, db)
-
+	if err != nil {
+		helper.ErrorResponse(respw, req, http.StatusInternalServerError, "Internal Server Error", "kesalahan server : get email "+err.Error())
+		return
+	}
+	
 
 	salt, err := hex.DecodeString(existsDoc.Salt)
 	if err != nil {
