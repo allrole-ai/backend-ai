7c09043120a050e8c9decc776f88c641848e05f2
fahad17 <fahadra96@gmail.com>
2024-07-30 23:12:58 +0700

    delelet hash password

--- controller/register.go
+++ controller/register.go
@@ -7,9 +7,7 @@
 
 
 
-	}
-	hashedPassword := argon2.IDKey([]byte(user.Password), salt, 1, 64*1024, 4, 32)
-	userData := bson.M{
+
 		"namalengkap": user.NamaLengkap,
 		"email":       user.Email,
 		"password":    hex.EncodeToString(hashedPassword),
