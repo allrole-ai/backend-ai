b4d089cdbc1995a8125cd4b15e020867bc90ad37
miqbalramadhan18 <miqbalr18@gmail.com>
2024-07-31 01:03:27 +0700

    Decode request body into AIRequest struct

--- controller/chat.go
+++ controller/chat.go
@@ -1,10 +1,14 @@
 package controller
 
 import (
+	"encoding/json"
 	"net/http"
+
+	"github.com/allrole-ai/backend-ai/model"
 )
 
 func Chat(respw http.ResponseWriter, req *http.Request, tokenmodel string) {
 	var chat model.AIRequest
-	
+	err := json.NewDecoder(req.Body).Decode(&chat)
+
 }
