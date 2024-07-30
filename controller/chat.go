753228ee99dad5c184e2ced1589309217abb8f60
miqbalramadhan18 <miqbalr18@gmail.com>
2024-07-31 00:47:26 +0700

    Create initial Login function structure

--- controller/chat.go
+++ controller/chat.go
@@ -1,1 +1,10 @@
 package controller
+
+import (
+	"net/http"
+
+	"go.mongodb.org/mongo-driver/mongo"
+)
+
+func Login(db *mongo.Database, respw http.ResponseWriter, req *http.Request, privatekey string) {}
+
