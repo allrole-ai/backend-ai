1a349553305c268772ff172873f6fe91a372b641
fahad17 <fahadra96@gmail.com>
2024-07-30 23:59:20 +0700

    delete decoder

--- controller/chat.go
+++ controller/chat.go
@@ -18,6 +18,3 @@ import (
 
 func Chat(respw http.ResponseWriter, req *http.Request, tokenmodel string) {
 	var chat model.AIRequest
-
-	err := json.NewDecoder(req.Body).Decode(&chat)
-	if err != nil {
