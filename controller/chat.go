2954e2938ec8a6152166f2edc629076bd6d0521f
miqbalramadhan18 <miqbalr18@gmail.com>
2024-07-31 01:05:03 +0700

    Add error handling for JSON decoding

--- controller/chat.go
+++ controller/chat.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"net/http"
 
+	"github.com/allrole-ai/backend-ai/helper"
 	"github.com/allrole-ai/backend-ai/model"
 )
 
@@ -11,4 +12,9 @@ func Chat(respw http.ResponseWriter, req *http.Request, tokenmodel string) {
 	var chat model.AIRequest
 	err := json.NewDecoder(req.Body).Decode(&chat)
 
+	if err != nil {
+		helper.ErrorResponse(respw, req, http.StatusBadRequest, "Bad Request", "error parsing request body "+err.Error())
+		return
+	}
+	
 }
