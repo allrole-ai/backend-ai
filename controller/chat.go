576231b72f7d349aca3e2d7e06f00bf5776a87b8
ryaasishlah <ryaasishlah123@gmail.com>
2024-07-31 01:36:53 +0700

    delete required packages

--- controller/chat.go
+++ controller/chat.go
@@ -10,11 +10,7 @@ import (
 	"strings"
 	"time"
 
-	"github.com/allrole-ai/backend-ai/config"
-	"github.com/allrole-ai/backend-ai/helper"
-	"github.com/allrole-ai/backend-ai/model"
-	"github.com/go-resty/resty/v2"
-)
+
 
 func Chat(respw http.ResponseWriter, req *http.Request, tokenmodel string) {
 	var chat model.AIRequest
