169d22916c26bf77987e0d7287c5bcd9c2f25c10
fahad17 <fahadra96@gmail.com>
2024-07-30 23:52:15 +0700

    delete resty

--- controller/chat.go
+++ controller/chat.go
@@ -27,8 +27,4 @@ func Chat(respw http.ResponseWriter, req *http.Request, tokenmodel string) {
 
 	if chat.Query == "" {
 		helper.ErrorResponse(respw, req, http.StatusBadRequest, "Bad Request", "mohon untuk melengkapi data")
-		return
-	}
-
-	client := resty.New()
 
