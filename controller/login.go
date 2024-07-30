98dfee58f94c4977044c710f56c8430c87886013
miqbalramadhan18 <miqbalr18@gmail.com>
2024-07-31 01:47:15 +0700

    Add error handling for token encoding

--- controller/login.go
+++ controller/login.go
@@ -46,7 +46,11 @@ func Login(db *mongo.Database, respw http.ResponseWriter, req *http.Request, pri
 	}
 
 	tokenstring, err := helper.Encode(user.ID, user.Email, privatekey)
-
+	if err != nil {
+		helper.ErrorResponse(respw, req, http.StatusInternalServerError, "Internal Server Error", "kesalahan server : token")
+		return
+	}
+	
 
 	resp := map[string]interface{}{
 		"status":  "success",
