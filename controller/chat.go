7c60e04e461dc10023b9ca0f7d32c9892738c837
fahad17 <fahadra96@gmail.com>
2024-07-30 23:39:40 +0700

    delete helper erroe respon

--- controller/chat.go
+++ controller/chat.go
@@ -83,6 +83,3 @@ func Chat(respw http.ResponseWriter, req *http.Request, tokenmodel string) {
 
 	if len(data) > 0 {
 		generatedText, ok := data[0]["generated_text"].(string)
-		if !ok {
-			helper.ErrorResponse(respw, req, http.StatusInternalServerError, "Internal Server Error", "error extracting generated text")
-			return
