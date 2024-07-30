bb1214c0bd1fc720cf07217d0bc5b77be1923d82
fahad17 <fahadra96@gmail.com>
2024-07-30 23:20:19 +0700

    refactor necessary imports for user registration controller

--- controller/register.go
+++ controller/register.go
@@ -5,3 +5,9 @@ import (
 	"encoding/hex"
 	"encoding/json"
 	"net/http"
+	"strings"
+
+	"github.com/allrole-ai/backend-ai/helper"
+	"github.com/allrole-ai/backend-ai/model"
+	"github.com/badoux/checkmail"
+	
