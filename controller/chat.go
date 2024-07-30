7cdc7ac646e9de32b829484960abf51253d5e224
ryaasishlah <ryaasishlah123@gmail.com>
2024-07-31 01:41:10 +0700

    delete Send request to Hugging Face API with retry logic

--- controller/chat.go
+++ controller/chat.go
@@ -20,11 +20,7 @@ package controller
 
 
 
-if response.StatusCode() == http.StatusOK {
-		break
-	} else {
-		var errorResponse map[string]interface{}
-		err = json.Unmarshal(response.Body(), &errorResponse)
+
 if err == nil && errorResponse["error"] == "Model is currently loading" {
 			retryCount++
 			time.Sleep(retryDelay)
