304a689d31603194b6b139bbc001e5674008c57d
miqbalramadhan18 <miqbalr18@gmail.com>
2024-07-31 01:31:09 +0700

    Decode request body into User struct

--- controller/login.go
+++ controller/login.go
@@ -15,7 +15,8 @@ import (
 // Login handles user login
 func Login(db *mongo.Database, respw http.ResponseWriter, req *http.Request, privatekey string) {}
 	var user model.User
-
+	if err := json.NewDecoder(req.Body).Decode(&user); err != nil {
+	
 
 	if user.Email == "" || user.Password == "" {
 		helper.ErrorResponse(respw, req, http.StatusBadRequest, "Bad Request", "mohon untuk melengkapi data")
