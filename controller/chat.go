096bb3d4f4466894c6ac7d56a64f00429ae43733
ryaasishlah <ryaasishlah123@gmail.com>
2024-07-31 01:42:02 +0700

    delete Send request to Hugging Face API with retry logic

--- controller/chat.go
+++ controller/chat.go
@@ -25,11 +25,7 @@ package controller
 
 
 
-			if !ok {
-				helper.ErrorResponse(respw, req, http.StatusInternalServerError, "Internal Server Error", "error extracting generated text")
-				return
-			}
-			helper.WriteJSON(respw, http.StatusOK, map[string]string{"answer": generatedText})
+
 	} else {
 		helper.ErrorResponse(respw, req, http.StatusInternalServerError, "Internal Server Error", "kesalahan server: response")
 	}
