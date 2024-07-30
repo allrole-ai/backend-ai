534147567a0ff44baf81061116d2c75c14d7012d
fahad17 <fahadra96@gmail.com>
2024-07-31 00:16:16 +0700

    Add Initialize variables for API request and retry logic

--- controller/chat.go
+++ controller/chat.go
@@ -38,3 +38,5 @@ func Chat(respw http.ResponseWriter, req *http.Request, tokenmodel string) {
 
 	var response *resty.Response
 	var retryCount int
+	maxRetries := 5
+	retryDelay := 20 * time.Second
