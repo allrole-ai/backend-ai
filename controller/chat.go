282e6f9e85b4fc63a454ef73d48e616a3d4ce456
fahad17 <fahadra96@gmail.com>
2024-07-31 00:21:49 +0700

    Extract generated text from API response and send it in the response, handle errors if extraction fails

--- controller/chat.go
+++ controller/chat.go
@@ -77,3 +77,9 @@ var data []map[string]interface{}
 
 	err = json.Unmarshal(response.Body(), &data)
 	if err != nil {
+		if len(data) > 0 {
+			generatedText, ok := data[0]["generated_text"].(string)
+			if !ok {
+				helper.ErrorResponse(respw, req, http.StatusInternalServerError, "Internal Server Error", "error extracting generated text")
+				return
+			}
