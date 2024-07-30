f087ba545988f03e33cc8d181aebe0f8dccd0a1b
miqbalramadhan18 <miqbalr18@gmail.com>
2024-07-31 01:59:26 +0700

    Declare User variable and decode request body

--- controller/register.go
+++ controller/register.go
@@ -16,6 +16,12 @@ import (
 )
 
 func Register(db *mongo.Database, col string, respw http.ResponseWriter, req *http.Request) {}
+	var user model.User
+	err := json.NewDecoder(req.Body).Decode(&user)
+	if err != nil {
+		helper.ErrorResponse(respw, req, http.StatusBadRequest, "Bad Request", "error parsing request body "+err.Error())
+		return
+	}
 
 
 	if user.NamaLengkap == "" || user.Email == "" || user.Password == "" || user.Confirmpassword == "" {
