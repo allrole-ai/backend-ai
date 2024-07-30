9f28f2b01359787c744373f218718fa3aeb66a0a
ryaasishlah <ryaasishlah123@gmail.com>
2024-07-31 01:38:24 +0700

    delete Decode request body to chat variable

--- controller/chat.go
+++ controller/chat.go
@@ -4,11 +4,7 @@ package controller
 
 
 
-	err := json.NewDecoder(req.Body).Decode(&chat)
-	if err != nil {
-		helper.ErrorResponse(respw, req, http.StatusBadRequest, "Bad Request", "error parsing request body "+err.Error())
-		return
-	}
+
 
 	if chat.Query == "" {
 		helper.ErrorResponse(respw, req, http.StatusBadRequest, "Bad Request", "mohon untuk melengkapi data")
