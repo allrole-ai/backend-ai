a87bcfd9f836125f31c4dea8ee454069fd5b37bd
fahad17 <fahadra96@gmail.com>
2024-07-30 23:49:55 +0700

    delete request hugging face

--- controller/chat.go
+++ controller/chat.go
@@ -46,10 +46,4 @@ func Chat(respw http.ResponseWriter, req *http.Request, tokenmodel string) {
 		response, err = client.R().
 			SetHeader("Authorization", apiToken).
 			SetHeader("Content-Type", "application/json").
-			SetBody(`{"inputs": "` + chat.Query + `"}`).
-			Post(apiUrl)
-
-		if err != nil {
-			log.Fatalf("Error making request: %v", err)
-		}
 
