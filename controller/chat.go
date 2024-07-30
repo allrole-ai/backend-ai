2edd001b544d8abc84587563b88e4d584f394fb2
ryaasishlah <ryaasishlah123@gmail.com>
2024-07-31 02:08:41 +0700

    update chat

--- controller/chat.go
+++ controller/chat.go
@@ -100,11 +100,4 @@ func Chat(respw http.ResponseWriter, req *http.Request, tokenmodel string) {
 	} else {
 		helper.ErrorResponse(respw, req, http.StatusInternalServerError, "Internal Server Error", "kesalahan server: response")
 	}
-
-	if response.StatusCode() != 200 {
-		helper.ErrorResponse(respw, req, http.StatusInternalServerError, "Internal Server Errorr", "error from Hugging Face API "+string(response.Body()))
-		return
-	}
-	var data []map[string]interface{}
-
 }
