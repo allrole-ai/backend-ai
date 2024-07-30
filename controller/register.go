e54db91865199ca77b6a401ba8209c2c0f3805cd
fahad17 <fahadra96@gmail.com>
2024-07-30 23:26:27 +0700

    Check if email already exists in the database

--- controller/register.go
+++ controller/register.go
@@ -32,3 +32,6 @@ func Register(db *mongo.Database, col string, respw http.ResponseWriter, req *ht
 		helper.ErrorResponse(respw, req, http.StatusBadRequest, "Bad Request", "email tidak valid")
 		return
 	}
+
+	userExists, _ := helper.GetUserFromEmail(user.Email, db)
+	if user.Email == userExists.Email {
