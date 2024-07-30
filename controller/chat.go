a7bcfbc6ef76f6ee2461d8b31873c9310edcabe0
miqbalramadhan18 <miqbalr18@gmail.com>
2024-07-31 00:48:00 +0700

    Add variable to decode request body

--- controller/chat.go
+++ controller/chat.go
@@ -3,8 +3,10 @@ package controller
 import (
 	"net/http"
 
+	"github.com/allrole-ai/backend-ai/model"
 	"go.mongodb.org/mongo-driver/mongo"
 )
 
 func Login(db *mongo.Database, respw http.ResponseWriter, req *http.Request, privatekey string) {}
+	var user model.User
 
