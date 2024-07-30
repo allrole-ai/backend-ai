0fc201c30afe51c859a3906359c0cda307b87d05
ryaasishlah <ryaasishlah123@gmail.com>
2024-07-31 01:51:51 +0700

    Split the URL path into segments and extract the model name

--- controller/chat.go
+++ controller/chat.go
@@ -39,3 +39,5 @@ func Chat(respw http.ResponseWriter, req *http.Request, tokenmodel string) {
 		helper.ErrorResponse(respw, req, http.StatusInternalServerError, "Internal Server Error", "error parsing URL model hugging face"+err.Error())
 		return
 	}
+	segments := strings.Split(parsedURL.Path, "/")
+	modelName := strings.Join(segments[2:], "/")
