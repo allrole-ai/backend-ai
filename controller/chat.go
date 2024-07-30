33e294154c4dda9a4bbbc5723efe650b017a55c2
fahad17 <fahadra96@gmail.com>
2024-07-31 00:12:45 +0700

    refactor Decode JSON request body into AIRequest model and handle errors

--- controller/chat.go
+++ controller/chat.go
@@ -20,3 +20,6 @@ func Chat(respw http.ResponseWriter, req *http.Request, tokenmodel string) {
 	var chat model.AIRequest
 	err := json.NewDecoder(req.Body).Decode(&chat)
 	if err != nil {
+		helper.ErrorResponse(respw, req, http.StatusBadRequest, "Bad Request", "error parsing request body "+err.Error())
+		return
+	}
