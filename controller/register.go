f81964a1d9d74a2eb61970f7ea9d7e8f5d988e92
fahad17 <fahadra96@gmail.com>
2024-07-30 23:09:01 +0700

    delelet encoder register

--- controller/register.go
+++ controller/register.go
@@ -4,11 +4,6 @@
 
 
 
-	err := json.NewDecoder(req.Body).Decode(&user)
-	if err != nil {
-		helper.ErrorResponse(respw, req, http.StatusBadRequest, "Bad Request", "error parsing request body "+err.Error())
-		return
-	}
 
 	if user.NamaLengkap == "" || user.Email == "" || user.Password == "" || user.Confirmpassword == "" {
 		helper.ErrorResponse(respw, req, http.StatusBadRequest, "Bad Request", "mohon untuk melengkapi data")
