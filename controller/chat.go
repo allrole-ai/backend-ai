2d1eb3abdaa9b283f306047efb72e3f44162c500
miqbalramadhan18 <miqbalr18@gmail.com>
2024-07-31 01:18:48 +0700

    Extract generated text from data

--- controller/chat.go
+++ controller/chat.go
@@ -72,6 +72,11 @@ func Chat(respw http.ResponseWriter, req *http.Request, tokenmodel string) {
 	err = json.Unmarshal(response.Body(), &data)
 
 	if len(data) > 0 {
+	generatedText, ok := data[0]["generated_text"].(string)
+	if !ok {
+    helper.ErrorResponse(respw, req, http.StatusInternalServerError, "Internal Server Error", "error extracting generated text")
+    return
+}
 
 
 }
