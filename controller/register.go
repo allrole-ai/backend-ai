1007f2529dfa905c0519ad6fd5d1e54fa8227923
fahad17 <fahadra96@gmail.com>
2024-07-30 23:12:38 +0700

    delelet status error respon

--- controller/register.go
+++ controller/register.go
@@ -6,11 +6,7 @@
 
 
 
-	salt := make([]byte, 16)
-	_, err = rand.Read(salt)
-	if err != nil {
-		helper.ErrorResponse(respw, req, http.StatusInternalServerError, "Internal Server Error", "kesalahan server : salt")
-		return
+
 	}
 	hashedPassword := argon2.IDKey([]byte(user.Password), salt, 1, 64*1024, 4, 32)
 	userData := bson.M{
