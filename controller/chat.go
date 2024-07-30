56298d6f52adb2ef0070975ce63104b1210a4bd0
ryaasishlah <ryaasishlah123@gmail.com>
2024-07-31 02:02:21 +0700

    Validate if chat query is empty

--- controller/chat.go
+++ controller/chat.go
@@ -21,3 +21,7 @@ func Chat(respw http.ResponseWriter, req *http.Request, tokenmodel string) {
 		helper.ErrorResponse(respw, req, http.StatusBadRequest, "Bad Request", "error parsing request body "+err.Error())
 		return
 	}
+	if chat.Query == "" {
+		helper.ErrorResponse(respw, req, http.StatusBadRequest, "Bad Request", "mohon untuk melengkapi data")
+		return
+	}
