60ea87b9c2e91014dc5182574590f95a99db1efd
fahad17 <fahadra96@gmail.com>
2024-07-30 23:48:45 +0700

    delete response status code

--- controller/chat.go
+++ controller/chat.go
@@ -53,7 +53,3 @@ func Chat(respw http.ResponseWriter, req *http.Request, tokenmodel string) {
 			log.Fatalf("Error making request: %v", err)
 		}
 
-		if response.StatusCode() == http.StatusOK {
-			break
-		} else {
-
