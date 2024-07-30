bd03eee54a212261ae52fdcef06adb27f06f9373
fahad17 <fahadra96@gmail.com>
2024-07-30 23:38:34 +0700

    delete helper error response

--- controller/chat.go
+++ controller/chat.go
@@ -89,6 +89,4 @@ func Chat(respw http.ResponseWriter, req *http.Request, tokenmodel string) {
 		}
 		helper.WriteJSON(respw, http.StatusOK, map[string]string{"answer": generatedText})
 	} else {
-		helper.ErrorResponse(respw, req, http.StatusInternalServerError, "Internal Server Error", "kesalahan server: response")
-	}
-}
+
