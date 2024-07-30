23562af780006e91cf357fcfbb2fcca17011013f
miqbalramadhan18 <miqbalr18@gmail.com>
2024-07-31 01:58:03 +0700

    delete

--- controller/register.go
+++ controller/register.go
@@ -15,13 +15,7 @@ import (
 	"golang.org/x/crypto/argon2"
 )
 
-func Register(db *mongo.Database, col string, respw http.ResponseWriter, req *http.Request) {
-	var user model.User
-	err := json.NewDecoder(req.Body).Decode(&user)
-	if err != nil {
-		helper.ErrorResponse(respw, req, http.StatusBadRequest, "Bad Request", "error parsing request body "+err.Error())
-		return
-	}
+
 
 	if user.NamaLengkap == "" || user.Email == "" || user.Password == "" || user.Confirmpassword == "" {
 		helper.ErrorResponse(respw, req, http.StatusBadRequest, "Bad Request", "mohon untuk melengkapi data")
