6188b0329af7d0e0e64ce4709638f1989e135f03
miqbalramadhan18 <miqbalr18@gmail.com>
2024-07-31 02:04:35 +0700

    delete

--- controller/register.go
+++ controller/register.go
@@ -39,16 +39,7 @@ func Register(db *mongo.Database, col string, respw http.ResponseWriter, req *ht
 		return
 	}
 
-
-	if strings.Contains(user.Password, " ") {
-		helper.ErrorResponse(respw, req, http.StatusBadRequest, "Bad Request", "password tidak boleh mengandung spasi")
-		return
-	}
-
-	if len(user.Password) < 8 {
-		helper.ErrorResponse(respw, req, http.StatusBadRequest, "Bad Request", "password minimal 8 karakter")
-		return
-	}
+	
 
 
 salt := make([]byte, 16)
