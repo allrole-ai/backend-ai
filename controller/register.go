84ea277319e9486508f18afcbb37887d05e91893
miqbalramadhan18 <miqbalr18@gmail.com>
2024-07-31 02:01:26 +0700

    Validate email format

--- controller/register.go
+++ controller/register.go
@@ -28,7 +28,10 @@ func Register(db *mongo.Database, col string, respw http.ResponseWriter, req *ht
 		return
 	}
 	
-
+	if err := checkmail.ValidateFormat(user.Email); err != nil {
+		helper.ErrorResponse(respw, req, http.StatusBadRequest, "Bad Request", "email tidak valid")
+		return
+	}	
 
 	userExists, _ := helper.GetUserFromEmail(user.Email, db)
 	if user.Email == userExists.Email {
