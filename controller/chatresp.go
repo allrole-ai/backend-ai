8dac57316c40da36fed4a1966025badda0dc0629
nizarabdulkholiq <kholiqnnabdul@gmail.com>
2024-07-31 17:32:25 +0700

    add new func

--- controller/chatresp.go
+++ controller/chatresp.go
@@ -206,16 +206,16 @@ func Chat(respw http.ResponseWriter, req *http.Request) {
 		}
 
 		if generatedText == "" {
-// 			WriteJSON(respw, http.StatusNoContent, map[string]string{"message": "No content generated"})
-// 		} else if generatedText == "special condition" { // Contoh kondisi khusus
-// 			WriteJSON(respw, http.StatusOK, map[string]string{"answer": generatedText, "note": "This is a special response"})
-// 		} else {
-// 			WriteJSON(respw, http.StatusOK, map[string]string{"answer": generatedText})
-// 		}
-// 	} else {
-// 		ErrorResponse(respw, req, http.StatusInternalServerError, "Internal Server Error", "kesalahan server: response")
-// 	}
-// }
+			WriteJSON(respw, http.StatusNoContent, map[string]string{"message": "No content generated"})
+		} else if generatedText == "special condition" { // Contoh kondisi khusus
+			WriteJSON(respw, http.StatusOK, map[string]string{"answer": generatedText, "note": "This is a special response"})
+		} else {
+			WriteJSON(respw, http.StatusOK, map[string]string{"answer": generatedText})
+		}
+	} else {
+		ErrorResponse(respw, req, http.StatusInternalServerError, "Internal Server Error", "kesalahan server: response")
+	}
+}
 
 // Fungsi untuk menangani respon JSON
 // func WriteJSON(w http.ResponseWriter, status int, data interface{}) {
