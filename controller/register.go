4c37a12d631d4ecdafb4ebf5924d8ae0aca98c64
FarhanRiziq01 <farhanriziq01@gmail.com>
2024-07-31 16:51:04 +0700

    delete import

--- controller/register.go
+++ controller/register.go
@@ -1,11 +1,6 @@
 package controller
 
-import (
-	"crypto/rand"
-	"encoding/hex"
-	"encoding/json"
-	"net/http"
-	"strings"
+
 
 	"github.com/allrole-ai/backend-ai/helper"
 	"github.com/allrole-ai/backend-ai/model"
