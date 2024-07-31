ca33eb49cf7d8ec4c5f5a436521dcbf0ea364630
nizarabdulkholiq <kholiqnnabdul@gmail.com>
2024-07-31 17:52:38 +0700

    update func

--- controller/resp.go
+++ controller/resp.go
@@ -115,79 +115,79 @@ func Chat(respw http.ResponseWriter, req *http.Request) {
 
 	var chat AIRequest
 	err := json.NewDecoder(req.Body).Decode(&chat)
-	if err != nil {
-		ErrorResponse(respw, req, http.StatusBadRequest, "Bad Request", "error parsing request body "+err.Error())
-		return
-	}
+	// if err != nil {
+	// 	ErrorResponse(respw, req, http.StatusBadRequest, "Bad Request", "error parsing request body "+err.Error())
+	// 	return
+	// }
 
-	if chat.Query == "" {
-		ErrorResponse(respw, req, http.StatusBadRequest, "Bad Request", "mohon untuk melengkapi data")
-		return
-	}
+	// if chat.Query == "" {
+	// 	ErrorResponse(respw, req, http.StatusBadRequest, "Bad Request", "mohon untuk melengkapi data")
+	// 	return
+	// }
 
-	client := resty.New()
-	apiUrl := "https://api-inference.huggingface.co/models/your_model_here" // Ganti dengan URL model Hugging Face yang sebenarnya
-	apiToken := "Bearer " + tokenmodel
+	// client := resty.New()
+	// apiUrl := "https://api-inference.huggingface.co/models/your_model_here" // Ganti dengan URL model Hugging Face yang sebenarnya
+	// apiToken := "Bearer " + tokenmodel
 
-	var response *resty.Response
-	var retryCount int
-	maxRetries := 5
-	retryDelay := 20 * time.Second
+	// var response *resty.Response
+	// var retryCount int
+	// maxRetries := 5
+	// retryDelay := 20 * time.Second
 
-	parsedURL, err := url.Parse(apiUrl)
-	if err != nil {
-		ErrorResponse(respw, req, http.StatusInternalServerError, "Internal Server Error", "error parsing URL model hugging face"+err.Error())
-		return
-	}
+	// parsedURL, err := url.Parse(apiUrl)
+	// if err != nil {
+	// 	ErrorResponse(respw, req, http.StatusInternalServerError, "Internal Server Error", "error parsing URL model hugging face"+err.Error())
+	// 	return
+	// }
 
-	segments := strings.Split(parsedURL.Path, "/")
-	modelName := strings.Join(segments[2:], "/")
+	// segments := strings.Split(parsedURL.Path, "/")
+	// modelName := strings.Join(segments[2:], "/")
 
-	for retryCount < maxRetries {
-		response, err = client.R().
-			SetHeader("Authorization", apiToken).
-			SetHeader("Content-Type", "application/json").
-			SetBody(`{"inputs": "` + chat.Query + `"}`).
-			Post(apiUrl)
+	// for retryCount < maxRetries {
+	// 	response, err = client.R().
+	// 		SetHeader("Authorization", apiToken).
+	// 		SetHeader("Content-Type", "application/json").
+	// 		SetBody(`{"inputs": "` + chat.Query + `"}`).
+	// 		Post(apiUrl)
 
-		if err != nil {
-			log.Fatalf("Error making request: %v", err)
-		}
+	// 	if err != nil {
+	// 		log.Fatalf("Error making request: %v", err)
+	// 	}
 
-		if response.StatusCode() == http.StatusOK {
-			break
-		} else {
-			var errorResponse map[string]interface{}
-			err = json.Unmarshal(response.Body(), &errorResponse)
-			if err == nil && errorResponse["error"] == "Model "+modelName+" is currently loading" {
-				retryCount++
-				time.Sleep(retryDelay)
-				continue
-			}
-			ErrorResponse(respw, req, http.StatusInternalServerError, "Internal Server Error", "error from Hugging Face API "+string(response.Body()))
-			return
-		}
-	}
+	// 	if response.StatusCode() == http.StatusOK {
+	// 		break
+	// 	} else {
+	// 		var errorResponse map[string]interface{}
+	// 		err = json.Unmarshal(response.Body(), &errorResponse)
+	// 		if err == nil && errorResponse["error"] == "Model "+modelName+" is currently loading" {
+	// 			retryCount++
+	// 			time.Sleep(retryDelay)
+	// 			continue
+	// 		}
+	// 		ErrorResponse(respw, req, http.StatusInternalServerError, "Internal Server Error", "error from Hugging Face API "+string(response.Body()))
+	// 		return
+	// 	}
+	// }
 
-	if response.StatusCode() != 200 {
-		ErrorResponse(respw, req, http.StatusInternalServerError, "Internal Server Error", "error from Hugging Face API "+string(response.Body()))
-		return
-	}
+	// if response.StatusCode() != 200 {
+	// 	ErrorResponse(respw, req, http.StatusInternalServerError, "Internal Server Error", "error from Hugging Face API "+string(response.Body()))
+	// 	return
+	// }
 
-	var data []map[string]interface{}
-	err = json.Unmarshal(response.Body(), &data)
-	if err != nil {
-		ErrorResponse(respw, req, http.StatusInternalServerError, "Internal Server Error", "error parsing response body "+err.Error())
-		return
-	}
+	// var data []map[string]interface{}
+	// err = json.Unmarshal(response.Body(), &data)
+	// if err != nil {
+	// 	ErrorResponse(respw, req, http.StatusInternalServerError, "Internal Server Error", "error parsing response body "+err.Error())
+	// 	return
+	// }
 
-	if len(data) > 0 {
-		generatedText, ok := data[0]["generated_text"].(string)
-		if !ok {
-			ErrorResponse(respw, req, http.StatusInternalServerError, "Internal Server Error", "error extracting generated text")
-			return
-		}
-
+	// if len(data) > 0 {
+	// 	generatedText, ok := data[0]["generated_text"].(string)
+	// 	if !ok {
+	// 		ErrorResponse(respw, req, http.StatusInternalServerError, "Internal Server Error", "error extracting generated text")
+	// 		return
+	// 	}
+/* 
 		if generatedText == "" {
 			WriteJSON(respw, http.StatusNoContent, map[string]string{"message": "No content generated"})
 		} else if generatedText == "special condition" { // Contoh kondisi khusus
@@ -198,4 +198,4 @@ func Chat(respw http.ResponseWriter, req *http.Request) {
 	} else {
 		ErrorResponse(respw, req, http.StatusInternalServerError, "Internal Server Error", "kesalahan server: response")
 	}
-}
+} */
