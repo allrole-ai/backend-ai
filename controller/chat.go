baec0f3430fcfba980fb1bff25522c6786448dd3
FarhanRiziq01 <farhanriziq01@gmail.com>
2024-07-31 17:11:24 +0700

    Parse Hugging Face API URL

--- controller/chat.go
+++ controller/chat.go
@@ -20,3 +20,18 @@ func Chat(respw http.ResponseWriter, req *http.Request, tokenmodel string) {
 	// Hugging Face API URL dan token
 	apiUrl := config.GetEnv("HUGGINGFACE_API_KEY")
 	apiToken := "Bearer " + tokenmodel
+	var response *resty.Response
+	var retryCount int
+	maxRetries := 5
+	retryDelay := 20 * time.Second
+
+	parsedURL, err := url.Parse(apiUrl)
+
+	if err != nil {
+		helper.ErrorResponse(respw, req, http.StatusInternalServerError, "Internal Server Error", "error parsing URL model hugging face"+err.Error())
+		return
+	}
+
+	segments := strings.Split(parsedURL.Path, "/")
+
+	modelName := strings.Join(segments[2:], "/")
