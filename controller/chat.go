74571a5293bee71566700a4e6f8f135eb9baa8eb
FarhanRiziq01 <farhanriziq01@gmail.com>
2024-07-31 17:08:54 +0700

    delete func

--- controller/chat.go
+++ controller/chat.go
@@ -1,20 +1,6 @@
 package controller
 
-func Chat(respw http.ResponseWriter, req *http.Request, tokenmodel string) {
-	var chat model.AIRequest
 
-	err := json.NewDecoder(req.Body).Decode(&chat)
-	if err != nil {
-		helper.ErrorResponse(respw, req, http.StatusBadRequest, "Bad Request", "error parsing request body "+err.Error())
-		return
-	}
-
-	if chat.Query == "" {
-		helper.ErrorResponse(respw, req, http.StatusBadRequest, "Bad Request", "mohon untuk melengkapi data")
-		return
-	}
-
-	client := resty.New()
 
 	// Hugging Face API URL dan token
 	apiUrl := config.GetEnv("HUGGINGFACE_API_KEY")
