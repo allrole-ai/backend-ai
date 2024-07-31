fae495b10dc99eb5a475e1d4ae8d99fd3c2d12d7
FarhanRiziq01 <farhanriziq01@gmail.com>
2024-07-31 16:55:19 +0700

    delete resp

--- controller/register.go
+++ controller/register.go
@@ -2,13 +2,5 @@ package controller
 
 
 
-
-	resp := map[string]any{
-		"message":    "berhasil mendaftar",
-		"insertedID": insertedID,
-		"data": map[string]string{
-			"email": user.Email,
-		},
-	}
 	helper.WriteJSON(respw, http.StatusCreated, resp)
 }
