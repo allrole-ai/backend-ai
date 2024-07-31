b3fb726c19ec6e9fff386ffbaf82f47d19c7fe34
FarhanRiziq01 <farhanriziq01@gmail.com>
2024-07-31 17:13:50 +0700

    Import Additional Packages

--- controller/chat.go
+++ controller/chat.go
@@ -1,5 +1,19 @@
 package controller
 
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+	"net/url"
+	"strings"
+	"time"
+
+	"github.com/allrole-ai/backend-ai/config"
+	"github.com/allrole-ai/backend-ai/helper"
+	"github.com/allrole-ai/backend-ai/model"
+	"github.com/go-resty/resty/v2"
+)
+
 func Chat(respw http.ResponseWriter, req *http.Request, tokenmodel string) {
 	var chat model.AIRequest
 
