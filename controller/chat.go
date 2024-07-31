23b9c7efde08b8fb5bceb60aaf1cac91d97d819f
FarhanRiziq01 <farhanriziq01@gmail.com>
2024-07-31 17:11:03 +0700

    Initialize Resty client

--- controller/chat.go
+++ controller/chat.go
@@ -15,3 +15,8 @@ func Chat(respw http.ResponseWriter, req *http.Request, tokenmodel string) {
 		helper.ErrorResponse(respw, req, http.StatusBadRequest, "Bad Request", "mohon untuk melengkapi data")
 		return
 	}
+	client := resty.New()
+
+	// Hugging Face API URL dan token
+	apiUrl := config.GetEnv("HUGGINGFACE_API_KEY")
+	apiToken := "Bearer " + tokenmodel
