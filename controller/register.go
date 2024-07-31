9fb5e1b8b2b3862d28011d95f622f40e7ad7a39c
FarhanRiziq01 <farhanriziq01@gmail.com>
2024-07-31 16:52:41 +0700

    delete user

--- controller/register.go
+++ controller/register.go
@@ -2,10 +2,6 @@ package controller
 
 
 
-	if user.NamaLengkap == "" || user.Email == "" || user.Password == "" || user.Confirmpassword == "" {
-		helper.ErrorResponse(respw, req, http.StatusBadRequest, "Bad Request", "mohon untuk melengkapi data")
-		return
-	}
 
 	if err := checkmail.ValidateFormat(user.Email); err != nil {
 		helper.ErrorResponse(respw, req, http.StatusBadRequest, "Bad Request", "email tidak valid")
