41b210fd93d769544577b61b40c6540a622d5aca
FarhanRiziq01 <farhanriziq01@gmail.com>
2024-07-31 17:00:41 +0700

    Prepare success response

--- controller/register.go
+++ controller/register.go
@@ -48,3 +48,10 @@ func Register(db *mongo.Database, col string, respw http.ResponseWriter, req *ht
 		helper.ErrorResponse(respw, req, http.StatusInternalServerError, "Internal Server Error", "kesalahan server : insert data, "+err.Error())
 		return
 	}
+	resp := map[string]any{
+		"message":    "berhasil mendaftar",
+		"insertedID": insertedID,
+		"data": map[string]string{
+			"email": user.Email,
+		},
+	}
