0741a1efc04e8c9600626e170df988f57a2287b6
FarhanRiziq01 <farhanriziq01@gmail.com>
2024-07-31 16:54:50 +0700

    delete bson

--- controller/register.go
+++ controller/register.go
@@ -3,12 +3,6 @@ package controller
 
 
 
-		"namalengkap": user.NamaLengkap,
-		"email":       user.Email,
-		"password":    hex.EncodeToString(hashedPassword),
-		"salt":        hex.EncodeToString(salt),
-	}
-
 	insertedID, err := helper.InsertOneDoc(db, col, userData)
 	if err != nil {
 		helper.ErrorResponse(respw, req, http.StatusInternalServerError, "Internal Server Error", "kesalahan server : insert data, "+err.Error())
