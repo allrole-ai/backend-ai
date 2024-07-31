90d523d8dd41b718a08702d2a431a98bc1e97531
nizarabdulkholiq <kholiqnnabdul@gmail.com>
2024-07-31 17:39:40 +0700

    add new func

--- controller/chatresp.go
+++ controller/chatresp.go
@@ -173,18 +173,18 @@ func Chat(respw http.ResponseWriter, req *http.Request) {
 
 		if response.StatusCode() == http.StatusOK {
 			break
-	// 	} else {
-	// 		var errorResponse map[string]interface{}
-	// 		err = json.Unmarshal(response.Body(), &errorResponse)
-	// 		if err == nil && errorResponse["error"] == "Model "+modelName+" is currently loading" {
-	// 			retryCount++
-	// 			time.Sleep(retryDelay)
-	// 			continue
-	// 		}
-	// 		ErrorResponse(respw, req, http.StatusInternalServerError, "Internal Server Error", "error from Hugging Face API "+string(response.Body()))
-	// 		return
-	// 	}
-	// }
+		} else {
+			var errorResponse map[string]interface{}
+			err = json.Unmarshal(response.Body(), &errorResponse)
+			if err == nil && errorResponse["error"] == "Model "+modelName+" is currently loading" {
+				retryCount++
+				time.Sleep(retryDelay)
+				continue
+			}
+			ErrorResponse(respw, req, http.StatusInternalServerError, "Internal Server Error", "error from Hugging Face API "+string(response.Body()))
+			return
+		}
+	}
 
 	// if response.StatusCode() != 200 {
 	// 	ErrorResponse(respw, req, http.StatusInternalServerError, "Internal Server Error", "error from Hugging Face API "+string(response.Body()))
