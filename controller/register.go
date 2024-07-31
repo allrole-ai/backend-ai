f6c69d733956b7699d564fb315e7fc5d21736fd1
FarhanRiziq01 <farhanriziq01@gmail.com>
2024-07-31 17:03:09 +0700

    Import checkmail and MongoDB packages

--- controller/register.go
+++ controller/register.go
@@ -3,11 +3,16 @@ package controller
 import (
 	"crypto/rand"
 	"encoding/hex"
+	"encoding/json"
 	"net/http"
 	"strings"
 
 	"github.com/allrole-ai/backend-ai/helper"
 	"github.com/allrole-ai/backend-ai/model"
+	"github.com/badoux/checkmail"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+	"golang.org/x/crypto/argon2"
 )
 
 func Register(db *mongo.Database, col string, respw http.ResponseWriter, req *http.Request) {
