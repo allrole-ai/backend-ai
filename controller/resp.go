b47d0f19bd84e37fb5555c7357b613a120e56351
nizarabdulkholiq <kholiqnnabdul@gmail.com>
2024-07-31 17:46:34 +0700

    update func

--- controller/resp.go
+++ controller/resp.go
@@ -119,14 +119,4 @@ func Chat(respw http.ResponseWriter, req *http.Request) {
 	maxRetries := 5
 	retryDelay := 20 * time.Second
 
-	segments := strings.Split(parsedURL.Path, "/")
-	modelName := strings.Join(segments[2:], "/")
-
-	for retryCount < maxRetries {
-		response, err = client.R().
-			SetHeader("Authorization", apiToken).
-			SetHeader("Content-Type", "application/json").
-			SetBody(`{"inputs": "` + chat.Query + `"}`).
-			Post(apiUrl)
-
-			
+	
