23d38d57414506530f3180a7ca6536bd6f12231a
FarhanRiziq01 <farhanriziq01@gmail.com>
2024-07-31 17:09:54 +0700

    delete req status

--- controller/chat.go
+++ controller/chat.go
@@ -3,18 +3,6 @@ package controller
 
 
 
-	if response.StatusCode() != 200 {
-		helper.ErrorResponse(respw, req, http.StatusInternalServerError, "Internal Server Errorr", "error from Hugging Face API "+string(response.Body()))
-		return
-	}
-
-	var data []map[string]interface{}
-
-	err = json.Unmarshal(response.Body(), &data)
-	if err != nil {
-		helper.ErrorResponse(respw, req, http.StatusInternalServerError, "Internal Server Error", "error parsing response body "+err.Error())
-		return
-	}
 
 	if len(data) > 0 {
 		generatedText, ok := data[0]["generated_text"].(string)
