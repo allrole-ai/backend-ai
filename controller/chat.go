585a2664acbca83c4167880a6f67c3ee04262d65
FarhanRiziq01 <farhanriziq01@gmail.com>
2024-07-31 17:12:56 +0700

    Extract generated text and send response

--- controller/chat.go
+++ controller/chat.go
@@ -1,7 +1,5 @@
 package controller
 
-
-
 func Chat(respw http.ResponseWriter, req *http.Request, tokenmodel string) {
 	var chat model.AIRequest
 
@@ -72,3 +70,14 @@ func Chat(respw http.ResponseWriter, req *http.Request, tokenmodel string) {
 		helper.ErrorResponse(respw, req, http.StatusInternalServerError, "Internal Server Error", "error parsing response body "+err.Error())
 		return
 	}
+	if len(data) > 0 {
+		generatedText, ok := data[0]["generated_text"].(string)
+		if !ok {
+			helper.ErrorResponse(respw, req, http.StatusInternalServerError, "Internal Server Error", "error extracting generated text")
+			return
+		}
+		helper.WriteJSON(respw, http.StatusOK, map[string]string{"answer": generatedText})
+	} else {
+		helper.ErrorResponse(respw, req, http.StatusInternalServerError, "Internal Server Error", "kesalahan server: response")
+	}
+}
