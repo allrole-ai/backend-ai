7d8a5e48922cd81e1b84db0631079002d4bff9c6
nizarabdulkholiq <kholiqnnabdul@gmail.com>
2024-07-31 17:36:05 +0700

    update func with new resp

--- controller/chatresp.go
+++ controller/chatresp.go
@@ -191,19 +191,19 @@ func Chat(respw http.ResponseWriter, req *http.Request) {
 		return
 	}
 
-	// var data []map[string]interface{}
-	// err = json.Unmarshal(response.Body(), &data)
-	// if err != nil {
-	// 	ErrorResponse(respw, req, http.StatusInternalServerError, "Internal Server Error", "error parsing response body "+err.Error())
-	// 	return
-	// }
+	var data []map[string]interface{}
+	err = json.Unmarshal(response.Body(), &data)
+	if err != nil {
+		ErrorResponse(respw, req, http.StatusInternalServerError, "Internal Server Error", "error parsing response body "+err.Error())
+		return
+	}
 
-	// if len(data) > 0 {
-	// 	generatedText, ok := data[0]["generated_text"].(string)
-	// 	if !ok {
-	// 		ErrorResponse(respw, req, http.StatusInternalServerError, "Internal Server Error", "error extracting generated text")
-	// 		return
-	// 	}
+	if len(data) > 0 {
+		generatedText, ok := data[0]["generated_text"].(string)
+		if !ok {
+			ErrorResponse(respw, req, http.StatusInternalServerError, "Internal Server Error", "error extracting generated text")
+			return
+		}
 
 // 		if generatedText == "" {
 // 			WriteJSON(respw, http.StatusNoContent, map[string]string{"message": "No content generated"})
